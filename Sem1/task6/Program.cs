333872090c0aabe8acd1b80ca420699c225bc23e
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 00:15:27 +0300

    Треугольник, но с ошибкой - не ясно

---
+++ Sem1/task6/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+int xa = 1;
+int ya = 1;
+int xb = 75;
+int yb = 1;
+int xc = 37;
+int yc = 12;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("8");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("8");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("8");
+int x = xa, y = ya;
+int count = 0;
+while(count<10000)
+{
+    int bust = new Random().Next(0,3);
+    if (bust == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    };
+    if (bust == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;
+    };
+    if (bust == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    };
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("8");
+    count++;
+};
+Console.SetCursorPosition(0, (yc+1));
