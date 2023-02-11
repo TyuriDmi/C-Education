be63c5e988c9e8321eaa5b3343cbcc826df2b58b
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-11 23:21:46 +0300

    Сложение двух случайных чисел

---
+++ Sem1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int num1 = new Random().Next(0,128);
+Console.Write("Первое число равно ");
+Console.WriteLine(num1);
+int num2 = new Random().Next(0,128);
+Console.Write("Второе число равно ");
+Console.WriteLine(num2);
+Console.Write("Сумма равна ");
+Console.WriteLine(num1+num2);
