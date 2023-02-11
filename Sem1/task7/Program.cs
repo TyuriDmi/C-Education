c818d67737803aac8819c98d5870b95e275967d2
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 00:55:51 +0300

    Квадрат числа

---
+++ Sem1/task7/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число, я порадую вас квадратом");
+double numb = Convert.ToDouble(Console.ReadLine());
+double Squ = numb*numb;
+Console.Write("Квадрат этого числа ");
+Console.WriteLine(Squ);
