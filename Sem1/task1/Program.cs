5a7b5cee1fcb1dfbf44c84268b38b174bb5e620b
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-11 23:08:34 +0300

    Приветствуем юзера

--- Sem1/task1/Program.cs
+++ Sem1/task1/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+Console.WriteLine("введите имя");
+
+string name = Console.ReadLine();
+
+Console.Write("Привет, ");
+Console.WriteLine(name);
