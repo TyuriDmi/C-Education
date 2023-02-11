6144287d706b847e8b24cb79b1a53c07c18e776e
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-11 23:07:30 +0300

    сложение двух числел

---
+++ Sem1/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1+num2;
+Console.Write("Сумма равна ");
+Console.WriteLine(sum);
+
