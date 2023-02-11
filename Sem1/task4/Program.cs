268e743263ca4a2f6b627862e8e688a1d70c4dc0
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-11 23:30:52 +0300

    особенное приветствие

---
+++ Sem1/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя");
+string name = Console.ReadLine();
+if (name.ToLower() == "дима")
+{
+    Console.WriteLine("Вот это да, это же целый ДИМА!");
+}
+else 
+{
+    Console.Write("Ну привет, ");
+    Console.WriteLine(name);
+}
