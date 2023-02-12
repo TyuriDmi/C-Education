4b8e0e2c957d440e680fc160fc0642929ea88cf1
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 13:03:39 +0300

    последняя цифра

---
+++ Sem1/task11/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int three = Convert.ToInt32(Console.ReadLine());
+Console.Write("Последняя цифра числа: ");
+int ost = three%10;
+Console.WriteLine(ost);
