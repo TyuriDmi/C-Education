910e8704520694168e80986fe8395e85152a1793
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 14:24:04 +0300

    Четные в диапазоне от 1 до введенного

---
+++ Homework1/Task4_AllEven/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.Write("Четные числа в диапазоне от 0 до ");
+Console.Write(number);
+Console.WriteLine(" следующие");
+while (i<number) {
+    if (i%2 ==0) {
+    Console.WriteLine(i);
+    i++;
+    }
+    else {i++;}
+}
