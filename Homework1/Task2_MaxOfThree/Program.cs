7807a7e6b8154929956538393ccc844d54eb7b2f
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 13:31:56 +0300

    Максимальное из трех

---
+++ Homework1/Task2_MaxOfThree/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите три числа");
+double FirstNumber = Convert.ToDouble(Console.ReadLine());
+double SecondNumber = Convert.ToDouble(Console.ReadLine());
+double ThirdNumber = Convert.ToDouble(Console.ReadLine());
+double max = FirstNumber;
+if (SecondNumber>max) {
+    max = SecondNumber;
+    if (ThirdNumber>max) {
+        max = ThirdNumber;
+    }
+}
+Console.Write("Максимальное из трех введенных чисел - ");
+Console.WriteLine(max);
