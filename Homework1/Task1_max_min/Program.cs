61ca55db9702f6f998d938de6c923782b0b20080
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 13:20:17 +0300

    Минимальное, максимальное

---
+++ Homework1/Task1_max_min/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число");
+double FirstNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double SecondNumber = Convert.ToDouble(Console.ReadLine());
+if (FirstNumber == SecondNumber) {
+    Console.WriteLine("Числа равны");
+}
+else {
+    if (FirstNumber > SecondNumber) {
+    Console.Write("Максимальное = ");
+    Console.WriteLine(FirstNumber);
+    Console.Write("Минимальное = ");
+    Console.WriteLine(SecondNumber);
+    }
+    else {
+    Console.Write("Максимальное = ");
+    Console.WriteLine(SecondNumber);
+    Console.Write("Минимальное = ");
+    Console.WriteLine(FirstNumber);
+    }
+}  
