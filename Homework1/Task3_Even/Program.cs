a21d1b9842444e508b066d097e7bb6bd8d842a3d
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 15:20:45 +0300

    Получилось реализовать проверку типа входящих данных

--- Homework1/Task3_Even/Program.cs
+++ Homework1/Task3_Even/Program.cs
@@ -1,16 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите целое число");
-//int temp = 0;
-//while (temp == 0) {
-int number = Convert.ToInt32(Console.ReadLine());
-//if (int.TryParse(number, out) == 1) {
-//    temp = 1;
-//        }
-//    else {
-//        Console.WriteLine("Введите корректное число");
-//    }
-//}
-if (number%2 == 0) {
+int temp = 0;
+int numb = 0;
+while (temp == 0) {
+string number = Console.ReadLine();
+bool read = Int32.TryParse(number, out numb);
+if (read == true) {
+    temp = 1;
+        }
+    else {
+        Console.WriteLine("Введите корректное число");
+    }
+}
+if (numb % 2 == 0) {
     Console.WriteLine("Ваше число четное");
 }
 else {
