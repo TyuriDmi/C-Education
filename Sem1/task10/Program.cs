dc74da7fc1f959d1319f927d9a374547782d4752
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-12 12:56:00 +0300

    Целые от -N до N

---
+++ Sem1/task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+double count = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Целые числа: ");
+for (double i = (-count); i<=count; i++)
+{
+Console.WriteLine(i);
+}
+
