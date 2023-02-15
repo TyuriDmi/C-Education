a96485faa25042a092548045c7accc7be2f26c95
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-15 20:47:58 +0300

    Поправил переменную флага в булевую

--- Homework2/Task1/Program.cs
+++ Homework2/Task1/Program.cs
@@ -1,13 +1,13 @@
-﻿int flag = 0;
+﻿bool flag = false;
 int number = -1;
-while (flag == 0) {
+while (flag == false) {
     Console.WriteLine("Введите трехзначное число");
     number = Convert.ToInt32 (Console.ReadLine());
     if (number/100 < 1 || number/100 > 9) 
     { 
         Console.WriteLine($"{number} не трехзначное!");
         }
-    else flag = 1;
+    else flag = true;
 }
 int SecondDigit = (number - (number % 10) - ((number /100) * 100))/10;
 Console.WriteLine ($"вторая цифра равна {SecondDigit}");
