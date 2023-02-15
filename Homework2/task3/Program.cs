17e24a0f292f2f15c34c249e7e24b9c7ee11662a
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-15 21:04:38 +0300

    Определение выходного по номеру

---
+++ Homework2/task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int DayNumber = Convert.ToInt32(Console.ReadLine());
+if (DayNumber>7 || DayNumber<1) {
+    Console.WriteLine ("нет такого дня недели");
+}
+else {
+    if (DayNumber == 6 || DayNumber == 7){
+        Console.WriteLine ("ВЫХОДНОЙ!");
+    }
+    else {
+        Console.WriteLine ("Работать.....");
+    }
+}
