3678a8570d5eb1d4e5d71e4aea408a65b14e8877
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-15 20:43:17 +0300

    Вторая цифра трехзначного

---
+++ Homework2/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int flag = 0;
+int number = -1;
+while (flag == 0) {
+    Console.WriteLine("Введите трехзначное число");
+    number = Convert.ToInt32 (Console.ReadLine());
+    if (number/100 < 1 || number/100 > 9) 
+    { 
+        Console.WriteLine($"{number} не трехзначное!");
+        }
+    else flag = 1;
+}
+int SecondDigit = (number - (number % 10) - ((number /100) * 100))/10;
+Console.WriteLine ($"вторая цифра равна {SecondDigit}");
