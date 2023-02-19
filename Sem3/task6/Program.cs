396e7a4ab4247edeb2d0fbba2d90be5d23eac0bf
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-19 18:03:02 +0300

    Кубы от 1 до N

---
+++ Sem3/task6/Program.cs
@@ -0,0 +1,23 @@
+﻿int EnteringInt (string mess) { 
+    Console.WriteLine (mess);
+    bool flag = false;
+    bool temp = false;
+    int coor = 0;
+    while (flag == false){
+        temp = Int32.TryParse(Console.ReadLine(), out coor);
+        if (temp == false){
+            Console.WriteLine($"Ошибка в введенных данных");
+        }
+        else  {
+            flag = true; 
+        }
+    }
+    return coor;
+}
+
+int Key = EnteringInt ("Введите число");
+double NewKey = Convert.ToDouble(Key);
+Console.WriteLine($"Кубы от 1 до {NewKey}: ");
+for (int i=1; i<NewKey; i++) {
+    Console.Write($"{Math.Pow(i,3)}  ");
+}
