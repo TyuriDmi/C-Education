18216b97e25008e10b7c4e0c3e876377783831a0
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-19 16:18:38 +0300

    Диапазон в зависимости от номера четверти

---
+++ Sem3/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+int EnteringInt (string mess) { 
+    Console.WriteLine (mess);
+    bool flag = false;
+    int Coor = 0;
+    while (flag == false) {
+        Coor = Convert.ToInt32 (Console.ReadLine());
+        if (Coor<1 || Coor>4) {
+            Console.WriteLine ("Введите корректное число");
+    }
+        else {
+            flag = true;
+        }
+    }
+    return Coor;
+}
+int Number = EnteringInt("Введите номер четверти");
+if (Number == 1) {Console.WriteLine("Возможные координаты: X>0; Y>0");}
+else if (Number == 2) {Console.WriteLine("Возможные координаты: X>0; Y<0");}
+else if (Number == 3) {Console.WriteLine("Возможные координаты: X<0; Y<0");}
+else if (Number == 4) {Console.WriteLine("Возможные координаты: X<0; Y>0");}
+
