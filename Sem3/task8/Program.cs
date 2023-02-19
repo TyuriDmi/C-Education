361c3ffb85991cfb10e7fcc48d9aa94d2d41302a
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-19 18:57:11 +0300

    Замена символов в тесте

---
+++ Sem3/task8/Program.cs
@@ -0,0 +1,13 @@
+﻿string Replace(string Text, char OldArg, char NewArg2) {
+string NewText = String.Empty;
+int length = Text.Length;
+for (int i = 0; i < length; i++) {
+    if (Text[i] == OldArg) {NewText = NewText + $"{NewArg2}";}
+    else {NewText = NewText + $"{Text[i]}";}
+    }
+return NewText;
+}
+
+Console.WriteLine("Введите текст");
+string NewLetters = Replace((Console.ReadLine()), 'f', 'F');
+Console.WriteLine(NewLetters);
