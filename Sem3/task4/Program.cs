f12422f64f0d06b7e72bfda4d2349400c81782d4
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-19 17:34:46 +0300

    Квадраты от 1 до N

---
+++ Sem3/task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int EnteringInt (string mess) { 
+    Console.WriteLine (mess);
+    bool flag = false;
+    bool temp = false;
+    int coor = 0;
+    while (flag == false){
+        temp = Int32.TryParse(Console.ReadLine(), out coor);
+        if (temp == false){
+            Console.WriteLine($"Ошибка в введенных данных");
+        }
+        else  {
+            flag = true; 
+        }
+    }
+    return coor;
+}
+
+int Key = EnteringInt ("Введите число");
+double NewKey = Convert.ToDouble(Key);
+for (int i=1; i<NewKey; i++) {
+    Console.WriteLine($"{i} в квадрате равно {Math.Pow(i,2)}");
+}
