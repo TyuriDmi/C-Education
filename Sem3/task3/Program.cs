aec4bb5d3d3844a4056ff0d8466f4f78a0944a6f
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-19 16:54:04 +0300

    Расстояние между точками

---
+++ Sem3/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿double EnteringCoor (string mess) { 
+    Console.WriteLine (mess);
+    double Coor = Convert.ToDouble(Console.ReadLine());
+    return Coor;
+}
+Double FirstX = EnteringCoor("Введите Х для 1-й точки");
+Double FirstY = EnteringCoor("Введите Y для 1-й точки");
+Double SecondX = EnteringCoor("Введите Х для 2-й точки");
+Double SecondY = EnteringCoor("Введите Y для 2-й точки");
+double distance = Math.Sqrt (Math.Pow((FirstX - SecondX),2) + Math.Pow((FirstY - SecondY),2));
+Console.WriteLine($"Расстояние между точками 1 и 2 равно {distance:f2}");
