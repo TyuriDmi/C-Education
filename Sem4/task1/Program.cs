2c233cd6030f3223f0cc4b2eb769175104aaed99
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 18:22:41 +0300

    Сумма от 1 до N

---
+++ Sem4/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int result = 0;
+int count = EnteringInt("ВВедите целое число");
+int i = 0;
+while (i<=count) {
+    result = result + i;
+    i++;
+}
+Console.WriteLine(result);
+
+
+
