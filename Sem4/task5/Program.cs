ba75cc8ad120bed212ee56e90e58bb1122f2f0fc
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 19:03:00 +0300

    возведение в натуральную степень

---
+++ Sem4/task5/Program.cs
@@ -0,0 +1,24 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int Var = EnteringInt("введите число");
+int Exl = EnteringInt("введите степень");
+int Result = Var;
+for (int i = 1; i<Exl; i++) {
+    Result = Result * Var;
+}
+Console.WriteLine(Result);
