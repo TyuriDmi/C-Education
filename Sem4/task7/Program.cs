e28743a73d0ccfd669e781da108145adf7239829
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 19:22:52 +0300

    Вывод элементов массива

---
+++ Sem4/task7/Program.cs
@@ -0,0 +1,27 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int length = Math.Abs(EnteringInt("введите количество элементов массива"));
+int[] Numbers = new int[length];
+for(int i=0; i<length; i++) {
+    Numbers[i] = EnteringInt($"введите {i+1} элемент массива");
+    }
+Console.Write("[");
+for(int j=0; j<length; j++) {
+    Console.Write($"{Numbers[j]}, ");
+}
+Console.Write("]");
