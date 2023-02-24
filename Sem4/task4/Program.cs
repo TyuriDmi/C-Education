dd9f4eae6b3137db568a122d94c48cfd313a50ed
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 18:53:33 +0300

    Заполнение массива случайными булями

---
+++ Sem4/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int length = Math.Abs(EnteringInt("введите количество элементов массива"));
+int[] bools = new int[length];
+for(int i=0; i<length; i++) {
+    bools[i] = Random.Shared.Next(0,2);
+    Console.Write($"{bools[i]}   " );
+}
+
