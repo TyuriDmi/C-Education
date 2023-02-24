61c4a6910c5d1d88db07b5db1349efdb97e4ec43
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 19:12:16 +0300

    сумма цифр в числе

---
+++ Sem4/task6/Program.cs
@@ -0,0 +1,25 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int Var = EnteringInt("Введите целое число");
+int i;
+int Result = 0;
+for (i = 1; Var>0; i++) {
+    Result = Result + Var%10;
+    Var = Var / 10;
+}
+Console.WriteLine(Result);
