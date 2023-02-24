e8cea54d56c8e49f30e8bdac4b5a5a94cec1c2f5
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-24 18:34:24 +0300

    Количество цифр в числе

---
+++ Sem4/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int EnteringInt(string MessageToUser) {
+bool flag = false;
+bool temp = false;
+int Number = 0;
+while (flag == false) {
+    Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+    if (temp == true) {
+        flag = true;
+    }
+    else {
+        Console.WriteLine("Ошибка ввода");
+    }
+}
+return Number;
+}
+
+int Var = EnteringInt("Введите целое число");
+int i;
+for (i = 1; Var>10; i++) {
+    Var = Var / 10;
+}
+Console.WriteLine(i);
