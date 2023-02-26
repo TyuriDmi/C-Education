52fcda91ad3034475e0efe7cfbb4c4ff1f2a222e
TyuriDmi <TyuriDmi1@gmail.com>
2023-02-26 15:59:52 +0300

    Количество элементов в заданом диапазоне

---
+++ Sem5/task4/Program.cs
@@ -0,0 +1,62 @@
+﻿int EnteringInt(string MessageToUser)
+{
+    bool flag = false;
+    bool temp = false;
+    int Number = 0;
+    while (flag == false)
+    {
+        Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+        if (temp == true)
+        {
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода");
+        }
+    }
+    return Number;
+}
+
+int[] FillArrayInt(int Length, int Type)
+{
+    int[] array = new int[Length];
+    if (Type == 0)
+    {
+        int LeftBorder = EnteringInt("введите левую границу случайного диапазона");
+        int RightBorder = EnteringInt("введите правую границу случайного диапазона");
+        for (int i = 0; i < Length; i++)
+        {
+            array[i] = Random.Shared.Next(LeftBorder, RightBorder);
+        }
+        return array;
+    }
+    else
+    {
+        for (int i = 0; i < Length; i++)
+        {
+            array[i] = EnteringInt($"Введите {i+1} элемент нового массива");
+        }
+        return array;
+    }
+}
+
+int InInterval(int[] array) {
+    int Result = 0;
+    int LeftBorder = EnteringInt("введите левую границу требуемого диапазона");
+    int RightBorder = EnteringInt("введите правую границу требуемого диапазона");
+    for(int i=0; i<array.Length; i++) {
+        if(array[i]<RightBorder+1 && array[i]>LeftBorder-1) {
+            Result +=1;
+        } 
+    }
+    return Result;
+}
+
+int Count = EnteringInt("Введите размер массива");
+int[] Test = FillArrayInt(Count, 0);
+Console.Write(string.Join(", ", Test));
+Console.WriteLine();
+Console.WriteLine($"В заданный диапазон попало {InInterval(Test)} элементов");
+
