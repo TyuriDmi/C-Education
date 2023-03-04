773f9d44dd66b047b8d83a6c47223c39fe1aff9e
TyuriDmi <TyuriDmi1@gmail.com>
2023-03-04 09:52:42 +0300

    Считает количество введенных положительных

---
+++ Sem6/task6/Program.cs
@@ -0,0 +1,35 @@
+﻿double EnteringDouble(string MessageToUser)
+{
+    bool flag = false;
+    bool temp = false;
+    double Number = 0;
+    while (flag == false)
+    {
+        Console.WriteLine(MessageToUser);
+        temp = double.TryParse((Console.ReadLine()), out Number);
+        if (temp == true)
+        {
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода");
+        }
+    }
+    return Number;
+}
+
+string temp = "";
+bool flag = true;
+int count = 0;
+Console.WriteLine($"Вводите любые числа. Как надоест, пишите stop и я посчитаю количество положительных");
+while(flag == true) {
+    temp = Console.ReadLine();
+    if(temp.ToLower() == "stop") {
+    flag = false;
+    break;}
+    else if (Convert.ToDouble(temp)>0) {
+        count++;
+    }
+}
+Console.WriteLine($"Введено {count} положительных");
