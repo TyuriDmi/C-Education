a1e8a5f0c70e82de89f59622c6201dffc7ae0537
TyuriDmi <TyuriDmi1@gmail.com>
2023-03-04 08:47:09 +0300

    Конвертация в двоичный

---
+++ Sem6/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿int EnteringInt(string MessageToUser)
+{
+    bool flag = false;
+    bool temp = false;
+    int Number = 0;
+    while (flag == false)
+    {
+        Console.Write(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+        if (temp == true)
+        {
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода");
+        }
+    }
+    return Number;
+}
+
+string ConvertDecimalToDual(int Number) {
+    string Dual = "";
+    while (Number>0) {
+        Dual = Convert.ToString(Number%2) + Dual;
+        Number = Number/2;
+    }
+    return Dual;
+}
+
+int Decimal = EnteringInt("Ваше число: ");
+string Result = ConvertDecimalToDual(Decimal);
+Console.WriteLine($"{Decimal} в двоичном коде = {Result}");
