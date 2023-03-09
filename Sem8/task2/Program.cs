a5ec955655af2055ba1f8f7e89fcf7596064627c
TyuriDmi <TyuriDmi1@gmail.com>
2023-03-09 19:46:21 +0300

    Разворот матрицы

---
+++ Sem8/task2/Program.cs
@@ -0,0 +1,76 @@
+﻿int EnteringInt(string MessageToUser)
+{
+    bool flag = false;
+    bool temp = false;
+    int Number = 0;
+    while (flag == false)
+    {
+        Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+        if (temp == true)
+        {
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода");
+        }
+    }
+    return Number;
+}
+
+void FillArrayManual(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); i++)
+        {
+            array[i, j] = EnteringInt(
+                $"Введите значение элемента массива c координатами {i} по горизонтали и {j} по вертикали"
+            );
+        }
+    }
+}
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Random.Shared.Next(-100, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.Write($"{array[i, array.GetLength(1)-1]}]");
+        Console.WriteLine();
+    }
+}
+
+int[,] ReverseArray(int[,] array) {
+    int[,] newArray = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i=0; i<array.GetLength(0); i++) {
+        for(int j=0; j<array.GetLength(1); j++) {
+            newArray[j,i] = array[i,j];
+        }
+    }
+    return newArray;
+}
+
+int NumberOfLines = EnteringInt("Введите количество строк");
+int NumberOfColumns = EnteringInt("Введите количество столбцов");
+int[,] MyArray = new int[NumberOfLines, NumberOfColumns];
+FillArrayRandom(MyArray);
+PrintArray(MyArray);
+Console.WriteLine("------------");
+PrintArray(ReverseArray(MyArray));
