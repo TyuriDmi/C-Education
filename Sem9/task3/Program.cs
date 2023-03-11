b0bc9903cb73103827c06d113f61e51c20433b1e
TyuriDmi <TyuriDmi1@gmail.com>
2023-03-11 20:08:59 +0300

    Сумма цифр

---
+++ Sem9/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int SummOfDigits (int number) {
+    int sum = number%10;
+    if (number<10) {
+        sum = number%10;
+        }
+    else {
+       sum = sum + SummOfDigits(number/10);
+    }
+    return sum;
+}
+
+Console.WriteLine($"{SummOfDigits(900)}");
+
+int SummOfDigits2 (int number) {
+    int sum = number%10;
+    if (number>10) {
+        sum = sum+SummOfDigits2(number/10);
+    }
+    return sum;
+}
+
+Console.WriteLine($"{SummOfDigits2(45012)}");
