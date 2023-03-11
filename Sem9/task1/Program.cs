9c578614a07a97a5f1b9233e8063bc097a73ae91
TyuriDmi <TyuriDmi1@gmail.com>
2023-03-11 19:12:42 +0300

    От 1 до N

---
+++ Sem9/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿int EnteringInt(string MessageToUser)
+{
+    bool flag = false;
+    bool temp = false;
+    int Number = 0;
+    while (flag == false)
+    {
+        Console.WriteLine(MessageToUser);
+        temp = Int32.TryParse((Console.ReadLine()), out Number);
+        if (temp == true)
+        {
+            flag = true;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода");
+        }
+    }
+    return Number;
+}
+
+void OneToN (int n) {
+    if (n>0) {
+        OneToN(n-1);
+        Console.WriteLine (n);
+    }
+    else return;
+}
+
+
+OneToN(EnteringInt("Введите число"));
